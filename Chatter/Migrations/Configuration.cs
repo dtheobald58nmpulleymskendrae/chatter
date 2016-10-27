86b6906fa1aa3ef23e66c9b15bbc9bb9cd9690a5
nmpulley <nmpulley@gmail.com>
2016-10-27 15:24:43 -0400

    Commit without other too-big repo

--- Chatter/Migrations/Configuration.cs
+++ Chatter/Migrations/Configuration.cs
@@ -4,29 +4,33 @@ namespace Chatter.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web;
+    using System.Collections.Generic;
+    using Models;
+    using static Models.ApplicationDbContext;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Chatter.Models.ChatterContext>
     {
+
+
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "Chatter.Models.ChatterContext";
         }
 
         protected override void Seed(Chatter.Models.ChatterContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            using (var db = new MyEntities())
+            {
+                var user1 = new User();
+                var user2 = new User();
+                var user3 = new User();
+                user1.Followers = new User[] { user2 };
+                user2.Followers = new User[] { user3 };
+                db.Users.Add(user1);
+                db.SaveChanges();
+            }
         }
     }
 }
--- Chatter/Models/IdentityModels.cs
+++ Chatter/Models/IdentityModels.cs
@@ -9,6 +9,8 @@ namespace Chatter.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -28,6 +30,19 @@ namespace Chatter.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+    public class MyEntities : DbContext
+        {
+            public DbSet<User> Users { get; set; }
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<User>()
+                    .HasMany(x => x.Followers).WithMany(x => x.Following)
+                    .Map(x => x.ToTable("Followers")
+                    .MapLeftKey("UserId")
+                    .MapRightKey("FollowerId"));
+                    }
         }
     }
 }
