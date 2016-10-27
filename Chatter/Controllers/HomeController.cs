2cbca4c0380ee93d1c67932fb1f4276b8ce1d6ef
dtheobald58 <dtheobald58@yahoo.com>
2016-10-27 16:26:29 -0400

    some home index formatting and added 3 google fonts

--- Chatter/Controllers/HomeController.cs
+++ Chatter/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using Chatter.Models;
 
 namespace Chatter.Controllers
 {
     public class HomeController : Controller
     {
+        private ChatterContext db = new ChatterContext();
         public ActionResult Index()
         {
             return View();
@@ -31,6 +34,28 @@ namespace Chatter.Controllers
         public ActionResult Page()
         {
             return View();
+        }
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Messages,Timestamp")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(message);
         }
     }
 }
