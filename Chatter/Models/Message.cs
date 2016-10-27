2eb8f33bed29231d0b55a30b543e13a5c5bc8b91
nmpulley <nmpulley@gmail.com>
2016-10-27 10:48:21 -0400

    initializing the program

---
+++ Chatter/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Chatter.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Messages { get; set; }
+        public DateTime Timestamp  { get; set; }
+}
+}
